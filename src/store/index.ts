aba9ed12790a4fcd2b8419f985368c4f80021b1e
kekee0724 <androidleo@outlook.com>
2021-12-10 01:14:35 +0800

    init

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,9 +1,19 @@
-import { state } from './state';
-import { createStore } from 'vuex';
+/*
+ * @Author: 可可同学
+ * @Date: 2021-12-10 00:32:42
+ * @LastEditTime: 2021-12-10 01:01:06
+ * @LastEditors: 可可同学
+ * @Description:
+ */
+import { state } from './state'
+import { createStore } from 'vuex'
 
 export default createStore({
   state,
-  mutations: {},
-  actions: {},
-  modules: {},
-});
+  mutations: {
+  },
+  actions: {
+  },
+  modules: {
+  }
+})
