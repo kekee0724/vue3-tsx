cae75c510a9258594ddddea23361f47f9ea2bacc
kekee0724 <androidleo@outlook.com>
2021-12-10 23:41:34 +0800

    vite-react

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,45 @@
-import { defineComponent } from 'vue';
-import {RouterLink, RouterView} from 'vue-router';
-import './style/main.scss'
+import { useState } from 'react'
+import logo from './logo.svg'
+import './App.css'
 
-export default defineComponent({
-  name: 'App',
-  setup() {
-    return () => (
-      <>
-        <div id="nav">
-          <RouterLink to="/">Home</RouterLink> |
-          <RouterLink to="/about">About</RouterLink>
-        </div>
-        <RouterView/>
-      </>
-    );
-  }
-});
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>Hello Vite + React!</p>
+        <p>
+          <button type="button" onClick={() => setCount((count) => count + 1)}>
+            count is: {count}
+          </button>
+        </p>
+        <p>
+          Edit <code>App.tsx</code> and save to test HMR updates.
+        </p>
+        <p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+          {' | '}
+          <a
+            className="App-link"
+            href="https://vitejs.dev/guide/features.html"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Vite Docs
+          </a>
+        </p>
+      </header>
+    </div>
+  )
+}
+
+export default App
--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,7 @@
-import { defineConfig } from 'vite';
-import vue from '@vitejs/plugin-vue'
-import vueJsx from '@vitejs/plugin-vue-jsx'
-import path from 'path';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
-const pathResolve = (pathStr: string) => {
-  return path.resolve(__dirname, pathStr);
-};
-
-module.exports = defineConfig({
-  plugins: [vue(), vueJsx()],
-  alias: {
-    '@': pathResolve('./src'),
-  },
-});
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()]
+})
