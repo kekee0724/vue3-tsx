d438c324430f6d5df687f4113168d18f22e6313b
kekee0724 <androidleo@outlook.com>
2021-12-11 13:15:50 +0800

    环境变量如何获取

--- src/apps/about/index.tsx
+++ src/apps/about/index.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { Button } from 'antd-mobile'
 
 export default function About() {
+  console.log('import.meta.env', import.meta.env)
   return <div>
     <Button color='primary'>About</Button>
   </div>
--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import config from './src/config'
 
+const env = process.argv[process.argv.length - 1]
+const base = config[env]
+console.log('process:::env', base)
 // https://vitejs.dev/config/
 export default defineConfig({
   server: {
