744c535857b96f558cb88ef4015e12ad6343494e
kekee0724 <androidleo@outlook.com>
2021-12-12 13:55:20 +0800

    过滤器nest g filter core/filter/http-exception

--- src/app.module.ts
+++ src/app.module.ts
@@ -10,7 +10,13 @@ import { PostsModule } from './posts/posts.module';
  * @Module() 装饰器接收四个属性：providers、controllers、imports、exports
  */
 @Module({
-  imports: [PostsModule, TypeOrmModule.forRoot()], // 导入模块的列表，如果需要使用其他模块的服务，需要通过这里导入
+  imports: [
+    PostsModule,
+    TypeOrmModule.forRoot({
+      //...
+      autoLoadEntities: true,
+    }),
+  ], // 导入模块的列表，如果需要使用其他模块的服务，需要通过这里导入
   controllers: [AppController], // 处理http请求，包括路由控制，向客户端返回响应，将具体业务逻辑委托给providers处理
   providers: [AppService], // 注入器实例化的提供者（服务提供者），处理具体的业务逻辑，各个模块之间可以共享
   exports: [], // 导出服务的列表，供其他模块导入使用。如果希望当前模块下的服务可以被其他模块共享，需要在这里配置导出
--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,7 @@
 import { NestFactory } from '@nestjs/core';
+
 import { AppModule } from './app.module';
+import { HttpExceptionFilter } from './core/filter/http-exception.filter';
 
 /**
  * 	应用程序的入口文件，它使用核心函数 NestFactory 来创建 Nest 应用程序的实例。
@@ -7,6 +9,8 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('mobile-api'); // 设置全局路由前缀
+  // 注册全局错误的过滤器
+  app.useGlobalFilters(new HttpExceptionFilter());
   await app.listen(8080);
 }
 bootstrap();
