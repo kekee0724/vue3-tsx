2b9ac011eacffbff6f4f9a6b2056566bb3cbffa8
kekee0724 <androidleo@outlook.com>
2021-12-13 00:47:41 +0800

    login

--- src/auth/local.strategy.ts
+++ src/auth/local.strategy.ts
@@ -6,11 +6,13 @@ import { Repository } from 'typeorm';
 import { BadRequestException } from '@nestjs/common';
 import { PassportStrategy } from '@nestjs/passport';
 import { InjectRepository } from '@nestjs/typeorm';
+import { AuthService } from './auth.service';
 
 export class LocalStorage extends PassportStrategy(Strategy) {
   constructor(
     @InjectRepository(User)
     private readonly userRepository: Repository<User>,
+    private readonly authService: AuthService,
   ) {
     super({
       usernameField: 'username',
@@ -33,6 +35,8 @@ export class LocalStorage extends PassportStrategy(Strategy) {
       throw new BadRequestException('密码错误！');
     }
 
-    return user;
+    const token = this.authService.login(user);
+    // return { ...user, ...token };
+    return token;
   }
 }
