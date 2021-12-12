6d1d66a0df63919528733f3725d467cae3025697
kekee0724 <androidleo@outlook.com>
2021-12-12 16:35:56 +0800

    nest g resource user

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,6 +4,7 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PostsModule } from './posts/posts.module';
+import { UserModule } from './user/user.module';
 
 /**
  * 应用程序的根模块(Module)
@@ -16,6 +17,7 @@ import { PostsModule } from './posts/posts.module';
       //...
       autoLoadEntities: true,
     }),
+    UserModule,
   ], // 导入模块的列表，如果需要使用其他模块的服务，需要通过这里导入
   controllers: [AppController], // 处理http请求，包括路由控制，向客户端返回响应，将具体业务逻辑委托给providers处理
   providers: [AppService], // 注入器实例化的提供者（服务提供者），处理具体的业务逻辑，各个模块之间可以共享
