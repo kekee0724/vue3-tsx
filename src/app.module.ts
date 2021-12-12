a77e54e16b68b28479416fd339d0841e0d564fde
kekee0724 <androidleo@outlook.com>
2021-12-12 18:41:15 +0800

    nest g resource auth

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,6 +5,7 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PostsModule } from './posts/posts.module';
 import { UserModule } from './user/user.module';
+import { AuthModule } from './auth/auth.module';
 
 /**
  * 应用程序的根模块(Module)
@@ -18,6 +19,7 @@ import { UserModule } from './user/user.module';
       autoLoadEntities: true,
     }),
     UserModule,
+    AuthModule,
   ], // 导入模块的列表，如果需要使用其他模块的服务，需要通过这里导入
   controllers: [AppController], // 处理http请求，包括路由控制，向客户端返回响应，将具体业务逻辑委托给providers处理
   providers: [AppService], // 注入器实例化的提供者（服务提供者），处理具体的业务逻辑，各个模块之间可以共享
