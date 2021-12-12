88eaf1cbdfb4ac3e8198c8ec024d4f2ad68db23c
kekee0724 <androidleo@outlook.com>
2021-12-12 21:15:33 +0800

    jwtModule

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -3,14 +3,20 @@ import { User } from 'src/user/entities/user.entity';
 import { Module } from '@nestjs/common';
 import { PassportModule } from '@nestjs/passport';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { JwtModule } from '@nestjs/jwt';
 
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 import { LocalStorage } from './local.strategy';
 
+const jwtModule = JwtModule.register({
+  secret: 'vgkEeveppBwCzPHr',
+  signOptions: { expiresIn: '4h' },
+});
 @Module({
-  imports: [TypeOrmModule.forFeature([User]), PassportModule],
+  imports: [TypeOrmModule.forFeature([User]), PassportModule, jwtModule],
   controllers: [AuthController],
   providers: [AuthService, LocalStorage],
+  exports: [jwtModule],
 })
 export class AuthModule {}
