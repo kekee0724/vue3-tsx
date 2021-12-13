4178c5724b8ca69935c938d1bd023b8b1fba0082
kekee0724 <androidleo@outlook.com>
2021-12-13 23:20:31 +0800

    --

--- src/apps/views/Setting.tsx
+++ src/apps/views/Setting.tsx
@@ -2,6 +2,12 @@ import { Options, Vue } from 'vue-class-component'
 import HelloWorld from '@levi-m/core-ui/components/HelloWorld.vue' // @ is an alias to /src
 import Hello from '@levi-m/core-ui/components/Hello.vue'
 
+const VueImpl = [
+  'data', 'beforeCreate', 'created', 'beforeMount',
+  'mounted', 'beforeUnmount', 'unmounted',
+  'beforeUpdate', 'updated', 'activated', 'deactivated',
+  'render', 'errorCaptured', 'serverPrefetch'
+];
 // @Options({
 //   components: {
 //     HelloWorld,
@@ -9,6 +15,16 @@ import Hello from '@levi-m/core-ui/components/Hello.vue'
 //   }
 // })
 export class Setting extends Vue {
+  beforeCreate() {
+    console.log("beforeCreate")
+  }
+  beforeMount() {
+    console.log(this)
+    console.log("beforeMount")
+  }
+  beforeUpdate() {
+    console.log("beforeUpdate")
+  }
   render() {
     return (
       <div class="home">
