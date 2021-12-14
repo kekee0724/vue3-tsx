86b9071416ae1c45df434fd8836f4b9ae666d65b
可可同学 <AndroidLeo@outlook.com>
2021-12-14 11:07:16 +0800

    这里的any用来定义props的类型，test接口用来定义this.state的类型

--- src/apps/demo/index.tsx
+++ src/apps/demo/index.tsx
@@ -6,10 +6,19 @@ import { Route, Switch, useRouteMatch } from 'react-router-dom';
 import About from '@levi-a/about';
 
 export { Nested } from './demo';
-export class Demo extends Component {
+//定义一个接口，目的是为后面的state提供类型，以便通过编译器的检查
+interface test {
+    name?: 'john',
+    age?: 18
+}
+//这里的any用来定义props的类型，test接口用来定义this.state的类型
+export class Demo extends Component<any, test>{
     constructor(props: any) {
         super(props);
-        this.state = {}
+        this.state = {
+            name: 'john',
+            age: 18
+        }
     }
 
     redirectHandle = () => {
