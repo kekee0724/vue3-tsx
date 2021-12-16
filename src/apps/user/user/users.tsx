a046b6cbc25f24272c4405934ee35d8be51fbd87
可可同学 <AndroidLeo@outlook.com>
2021-12-16 18:20:28 +0800

    --

--- src/apps/user/user/users.tsx
+++ src/apps/user/user/users.tsx
@@ -5,63 +5,83 @@ import React, {
   useState,
 } from 'react';
 
-import { Dialog, Image, List, SwipeAction } from 'antd-mobile';
+import { Dialog, Image, List, SwipeAction, Badge } from 'antd-mobile';
 import { Action } from 'antd-mobile/es/components/swipe-action';
 import { connect } from 'dva';
 
-const users = [
-  {
-    avatar:
-      'https://images.unsplash.com/photo-1548532928-b34e3be62fc6?ixlib=rb-1.2.1&q=80&fm=jpg&crop=faces&fit=crop&h=200&w=200&ixid=eyJhcHBfaWQiOjE3Nzg0fQ',
-    name: 'Novalee Spicer',
-    description: 'Deserunt dolor ea eaque eos',
-  },
-  {
-    avatar:
-      'https://images.unsplash.com/photo-1493666438817-866a91353ca9?ixlib=rb-0.3.5&q=80&fm=jpg&crop=faces&fit=crop&h=200&w=200&s=b616b2c5b373a80ffc9636ba24f7a4a9',
-    name: 'Sara Koivisto',
-    description: 'Animi eius expedita, explicabo',
-  },
-  {
-    avatar:
-      'https://images.unsplash.com/photo-1542624937-8d1e9f53c1b9?ixlib=rb-1.2.1&q=80&fm=jpg&crop=faces&fit=crop&h=200&w=200&ixid=eyJhcHBfaWQiOjE3Nzg0fQ',
-    name: 'Marco Gregg',
-    description: 'Ab animi cumque eveniet ex harum nam odio omnis',
-  },
-  {
-    avatar:
-      'https://images.unsplash.com/photo-1546967191-fdfb13ed6b1e?ixlib=rb-1.2.1&q=80&fm=jpg&crop=faces&fit=crop&h=200&w=200&ixid=eyJhcHBfaWQiOjE3Nzg0fQ',
-    name: 'Edith Koenig',
-    description: 'Commodi earum exercitationem id numquam vitae',
-  },
-]
-
-export const Users = connect(mapStateToProps)((props: any) => {
-  const { countNamespace: state } = props;
-  console.log(props, state)
+export const Users = connect(mapStateToProps)(({ list: dataSource, total, page: current }: any) => {
+  // const { countNamespace: state } = props;
+  console.log(dataSource, total, current)
+  const leftActions: (item: any) => Action[] = (item: any) => {
+    return [
+      {
+        key: 'pin',
+        text: '置顶',
+        color: 'primary',
+        onClick: async () => {
+          await Dialog.confirm({
+            content: '确定要置顶吗？',
+          })
+          console.log(item.id)
+        },
+      },
+    ]
+  }
+  const rightActions: (item: any) => Action[] = (item: any) => [
+    // {
+    //   key: 'unsubscribe',
+    //   text: '取消关注',
+    //   color: 'light',
+    // },
+    {
+      key: 'mute',
+      text: '编辑',
+      color: 'warning',
+      onClick: async () => {
+        await Dialog.confirm({
+          content: '确定要编辑吗？',
+        })
+        console.log(item.id)
+      },
+    },
+    {
+      key: 'delete',
+      text: '删除',
+      color: 'danger',
+      onClick: async (e: any) => {
+        await Dialog.confirm({
+          content: '确定要删除吗？',
+        })
+        console.log(item.id)
+      },
+    },
+  ]
   return (
     <>
-      <DemoBlock title='用户列表' padding='0' border='0'>
+      {/* <DemoBlock title='用户列表' padding='0' border='0'>
         <WithList />
-      </DemoBlock>
+      </DemoBlock> */}
       <DemoBlock title='用户列表' padding='0' border='none'>
         <List>
-          {users.map(user => (
-            <List.Item
-              key={user.name}
-              prefix={
-                <Image
-                  src={user.avatar}
-                  style={{ borderRadius: 20 }}
-                  fit='cover'
-                  width={40}
-                  height={40}
-                />
-              }
-              description={user.description}
+          {dataSource.map((user: { id: number, name: string, username: string, email: string, website: string }) => (
+            <SwipeAction
+              key={user.email}
+              leftActions={leftActions(user)}
+              rightActions={rightActions(user)}
             >
-              {user.name}
-            </List.Item>
+              <List.Item
+                prefix={
+                  <div className="comp-box margin-v-xs">
+                    <Badge>
+                      <div>{user.username?.slice(0, 4)}</div>
+                    </Badge>
+                  </div>
+                }
+                description={<><div>{user.website}</div><div>{user.email}</div></>}
+              >
+                {user.name}
+              </List.Item>
+            </SwipeAction>
           ))}
         </List>
       </DemoBlock>
@@ -77,8 +97,13 @@ export const Users = connect(mapStateToProps)((props: any) => {
   );
 })
 
-function mapStateToProps() {
-  return {};
+function mapStateToProps(state: { users: { list: any; total: any; page: any; }; }) {
+  const { list, total, page } = state.users;
+  return {
+    list,
+    total,
+    page,
+  };
 }
 
 // 配合列表使用
@@ -150,7 +175,6 @@ const DemoBlock: React.FC<DemoBlockProps> = (props) => {
     setCount(count + 1)
   }
   const { title, padding, border, children } = props;
-  console.log(title, padding, border, children)
   return <div>
     {/* <p>我是message页面,name是：{name}</p>
     <p>我是message页面,count是：{count}</p>
@@ -158,9 +182,9 @@ const DemoBlock: React.FC<DemoBlockProps> = (props) => {
     <br />
     <button onClick={addCount}>点击我改变count</button> */}
     <div className="demoBlock">
-      <div className="title">{props.title}</div>
-      <div className="content" style={{ padding: (props.padding || 0) + "px", background: "rgb(255, 255, 255)", border: (props.border || 0) + "px" || "none" }}>
-        {props.children}
+      <div className="title">{title}</div>
+      <div className="content" style={{ padding: (padding || 0) + "px", background: "rgb(255, 255, 255)", border: (border || 0) + "px" || "none" }}>
+        {children}
       </div>
     </div>
   </div>;
