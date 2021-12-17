39d4e04b9adb13acb1b082ff174e91bbe6b945f1
可可同学 <AndroidLeo@outlook.com>
2021-12-17 11:06:16 +0800

    --

--- src/apps/views/About.tsx
+++ src/apps/views/About.tsx
@@ -1,4 +1,4 @@
-import { defineComponent } from "vue";
+import { computed, defineComponent, getCurrentInstance, reactive, ref, isRef, toRefs, onBeforeUnmount, onMounted } from "vue";
 
 import Logo from "@/assets/images/logo.png";
 import { HelloWord } from "@levi-m/core-ui";
@@ -6,12 +6,43 @@ import { HelloWord } from "@levi-m/core-ui";
 export const About = defineComponent({
   name: 'App',
   setup() {
+    //生命周期
+    onMounted(() => {
+      console.log("onMounted");
+    })
+    onBeforeUnmount(() => {
+      console.log("onBeforeUnmount");
+    })
+    const res = reactive({ name: '可可' }); //定义响应式数据，针对对象
     return () => (
       <>
         <h1>About.tsx</h1>
-        <img src={Logo} />
+        <h1>{res.name}</h1>
+        {/* <img src={Logo} /> */}
         <HelloWord />
       </>
     );
+  }
+});
+
+export const AboutV = defineComponent({
+  setup(props, context) {
+    //生命周期
+    onMounted(() => {
+      console.log("onMounted");
+    })
+    onBeforeUnmount(() => {
+      console.log("onBeforeUnmount");
+    })
+    const res = reactive({ 'name': 'zhuy' }); //定义响应式数据，针对对象
+    const count = ref(0); //定义基本类型数据
+    const obj = toRefs(res); //响应式数据还原成对象，主要解决响应式数据结构或其它操作出现的问题。
+    // const name = computed(() => context.$store.state.res.name);
+    return {
+      res,
+      count,
+      obj,
+      name
+    }
   }
 });
