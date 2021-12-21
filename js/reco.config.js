ba950d6f0df32c1fd2f2800b48cf26360a6a6421
可可同学 <AndroidLeo@outlook.com>
2021-12-21 11:03:52 +0800

    reco

---
+++ js/reco.config.js
@@ -0,0 +1,16 @@
+var client = {
+  title: 'iPark+下载页面',
+  techSupport: '上海拜特信息技术有限公司 版权所有',
+  appName: 'iPark<sup>+</sup>',
+  appTitle: '园企互动平台',
+  bgImg: "background-image: url('images/contentbg.png')",
+  bgColor: 'background-color: #02b8cd;',
+};
+
+var server = {
+  AppDownUrl:
+    'http://dev.bitech.cn/biparke01-v41-api/Operation/Operation/AppDown',
+  // 旧的iOS跳转配置
+  iosHref:
+    'itms-services://?action=download-manifest&url=https://assets.bitech.cn/mobile/ios/ipark+/alphajava/index.html',
+};
